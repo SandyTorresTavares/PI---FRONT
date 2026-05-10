b4e90f43458632ba16d1170806e6edd2aa26bcd9
SandyTorresTavares <sandyt.tavares@gmail.com>
2026-05-10 17:02:16 -0300

    useexport false in local

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const isExport = process.env.USE_EXPORT === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/PI---FRONT',
-  assetPrefix: '/PI---FRONT/',
+  ...(isExport && {
+    output: 'export',
+    basePath: '/PI---FRONT',
+    assetPrefix: '/PI---FRONT/',
+  }),
 };
 
 module.exports = nextConfig;
