717a7535b99791cca694ea9001d736f116a12cd0
SandyTorresTavares <sandyt.tavares@gmail.com>
2026-05-10 15:43:28 -0300

    fix: ambiente estático

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isExport = process.env.USE_EXPORT === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
+  ...(isExport && {
+    output: 'export',
+    basePath: '/PI---FRONT',
+    assetPrefix: '/PI---FRONT/',
+  }),
 };
 
 module.exports = nextConfig;
